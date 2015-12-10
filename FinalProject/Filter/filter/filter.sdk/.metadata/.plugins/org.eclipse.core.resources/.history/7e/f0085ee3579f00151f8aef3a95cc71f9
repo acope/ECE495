31e7d6a7e0e4d709794ef4c4c5f4af8d784db767
Austin <copeman.austin@gmail.com>
2015-12-10 14:32:15 -0500

    Final Project

---
+++ FinalProject/Filter/filter/filter.sdk/.metadata/.plugins/org.eclipse.core.resources/.history/7e/f0085ee3579f00151f8aef3a95cc71f9
@@ -0,0 +1,16 @@
+/*
+ * Grayscale.h
+ *
+ *  Created on: Dec 10, 2015
+ *      Author: mr_co_000
+ */
+
+#ifndef GRAYSCALE_H_
+#define GRAYSCALE_H_
+
+
+void grayscaleFilter(void);
+
+
+
+#endif /* GRAYSCALE_H_ */
--- FinalProject/Filter/filter/filter.sdk/filter2/Debug/src/main.d
+++ FinalProject/Filter/filter/filter.sdk/filter2/Debug/src/main.d
@@ -5,14 +5,7 @@ src/main.o src/main.d: ../src/main.c \
  ../../filter2_bsp/ps7_cortexa9_0/include/xil_assert.h \
  ../../filter2_bsp/ps7_cortexa9_0/include/xparameters.h \
  ../../filter2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
- ../../filter2_bsp/ps7_cortexa9_0/include/xil_printf.h \
- ../../filter2_bsp/ps7_cortexa9_0/include/xparameters.h \
- ../../filter2_bsp/ps7_cortexa9_0/include/xil_io.h \
- ../../filter2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
- ../../filter2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
- ../../filter2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
- ../../filter2_bsp/ps7_cortexa9_0/include/xil_printf.h \
- ../../filter2_bsp/ps7_cortexa9_0/include/xuartlite.h
+ ../src/Grayscale.h
 
 ../../filter2_bsp/ps7_cortexa9_0/include/myGrayscale.h:
 
@@ -26,18 +19,4 @@ src/main.o src/main.d: ../src/main.c \
 
 ../../filter2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
 
-../../filter2_bsp/ps7_cortexa9_0/include/xil_printf.h:
-
-../../filter2_bsp/ps7_cortexa9_0/include/xparameters.h:
-
-../../filter2_bsp/ps7_cortexa9_0/include/xil_io.h:
-
-../../filter2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
-
-../../filter2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
-
-../../filter2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
-
-../../filter2_bsp/ps7_cortexa9_0/include/xil_printf.h:
-
-../../filter2_bsp/ps7_cortexa9_0/include/xuartlite.h:
+../src/Grayscale.h:
